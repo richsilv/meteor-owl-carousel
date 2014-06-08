867c90cbfbd105d3d453ffe261d239d1227b13dd
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-06-08 22:01:33 +0100

    amended smart.json

--- package.js
+++ package.js
@@ -6,8 +6,6 @@ Package.on_use(function (api) {
 
   api.use('jquery','client');
 
-  console.log("hello!");
-
   api.add_files('lib/owl.carousel.css', 'client');
   api.add_files('lib/owl.theme.default.css', 'client');
   api.add_files('lib/owl.carousel.js', 'client');
